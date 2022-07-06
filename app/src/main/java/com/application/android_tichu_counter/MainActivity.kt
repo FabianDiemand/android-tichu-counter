613c5db3e957caf0cd56709a77f17f1eda87a586
Fabian Diemand <fabian.diemand@swisscom.com>
2022-07-07 00:47:46 +0200

    feature(main-activity): prepare main activity logic

--- app/src/main/java/com/application/android_tichu_counter/MainActivity.kt
+++ app/src/main/java/com/application/android_tichu_counter/MainActivity.kt
@@ -2,10 +2,51 @@ package com.application.android_tichu_counter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bNewGame: MaterialButton
+    private lateinit var bLoadGame: MaterialButton
+    private lateinit var bSettings: MaterialButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bNewGame = findViewById(R.id.mb_newgame)
+        bLoadGame = findViewById(R.id.mb_loadgame)
+        bSettings = findViewById(R.id.mb_settings)
+
+        setOnClickListeners()
     }
+
+    private fun setOnClickListeners(){
+        bNewGame.setOnClickListener {
+            startGame(it)
+        }
+
+        bLoadGame.setOnClickListener {
+            loadGame(it)
+        }
+
+        bNewGame.setOnClickListener {
+            startSettings(it)
+        }
+    }
+
+    private fun startGame(view: View){
+
+    }
+
+    private fun loadGame(view: View){
+
+    }
+
+    private fun startSettings(view: View){
+
+    }
+
 }
