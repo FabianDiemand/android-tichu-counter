11c8480041b11efd74c020fcd62729ee8ac3d90a
Fabian Diemand <fabian.diemand@swisscom.com>
2022-07-15 17:34:46 +0200

    feature(main-activity): create dialog to enter team names, replace svg files by pngs

--- app/src/main/java/com/application/android_tichu_counter/MainActivity.kt
+++ app/src/main/java/com/application/android_tichu_counter/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.application.android_tichu_counter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import com.application.android_tichu_counter.fragments.TeamNameDialogFragment
 import com.google.android.material.button.MaterialButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TeamNameDialogFragment.TeamNameDialogListener {
+    private var TAG = "MainActivity"
 
     private lateinit var bNewGame: MaterialButton
     private lateinit var bLoadGame: MaterialButton
@@ -13,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -21,32 +27,63 @@ class MainActivity : AppCompatActivity() {
         bSettings = findViewById(R.id.mb_settings)
 
         setOnClickListeners()
+
+        Log.d(TAG, "Create view.")
     }
 
     private fun setOnClickListeners(){
         bNewGame.setOnClickListener {
-            startGame(it)
+            startGame()
         }
 
         bLoadGame.setOnClickListener {
-            loadGame(it)
+            loadGame()
         }
 
-        bNewGame.setOnClickListener {
-            startSettings(it)
+        bSettings.setOnClickListener {
+            startSettings()
         }
+
+        Log.d(TAG, "Set OnClickListeners")
     }
 
-    private fun startGame(view: View){
+    private fun startGame(){
+        Toast.makeText(this, "New Game", Toast.LENGTH_SHORT).show()
+        showTeamNameDialog()
 
+        Log.d(TAG, "Start Game clicked.")
     }
 
-    private fun loadGame(view: View){
-
+    private fun loadGame(){
+        Toast.makeText(this, "Load Game", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "Load Game clicked.")
     }
 
-    private fun startSettings(view: View){
+    private fun startSettings(){
+        Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
 
+        Log.d(TAG, "Settings clicked.")
     }
 
+    private fun showTeamNameDialog(){
+        val dialog = TeamNameDialogFragment()
+        dialog.show(supportFragmentManager, "TeamNameDialogFragment")
+
+        Log.d(TAG, "Showing team name dialog.")
+    }
+
+    override fun onDialogPositiveClick(dialog: DialogFragment) {
+        val team1 = dialog.dialog?.findViewById<EditText>(R.id.et_dialog_name_team1)?.text.toString()
+        val team2 = dialog.dialog?.findViewById<EditText>(R.id.et_dialog_name_team2)?.text.toString()
+
+        Toast.makeText(this, "$team1 $team2", Toast.LENGTH_SHORT).show()
+
+        Log.d(TAG, "Save team names clicked.")
+    }
+
+    override fun onDialogNegativeClick(dialog: DialogFragment) {
+        dialog.dismiss()
+
+        Log.d(TAG, "Dialog dismissed.")
+    }
 }
---
+++ app/src/main/java/com/application/android_tichu_counter/fragments/TeamNameDialogFragment.kt
@@ -0,0 +1,64 @@
+package com.application.android_tichu_counter.fragments
+
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.application.android_tichu_counter.R
+import com.google.android.material.button.MaterialButton
+
+class TeamNameDialogFragment : DialogFragment() {
+    private var TAG = "TeamNameDialogFragment"
+
+    private lateinit var listener: TeamNameDialogListener
+
+    interface TeamNameDialogListener{
+        fun onDialogPositiveClick(dialog: DialogFragment)
+        fun onDialogNegativeClick(dialog: DialogFragment)
+    }
+
+    override fun onAttach(context: Context){
+        super.onAttach(context)
+        Log.d(TAG, "Dialog attached.")
+
+        try{
+            listener = context as TeamNameDialogListener
+            Log.d(TAG, "Listener instantiated.")
+        } catch(e: ClassCastException){
+            throw ClassCastException((context.toString() + "must implement TeamNameDialogListener."))
+        }
+    }
+
+    @SuppressLint("InflateParams")
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        Log.d(TAG, "Create dialog.")
+
+        return activity?.let{
+            val builder = AlertDialog.Builder(it)
+            val dialogView = LayoutInflater.from(context).inflate(R.layout.dialog_teamnames, null, false)
+
+            val bCancel = dialogView.findViewById<MaterialButton>(R.id.b_cancelmission)
+            val bSave = dialogView.findViewById<MaterialButton>(R.id.b_save_teamnames)
+
+            bCancel.setOnClickListener {
+                Toast.makeText(context, "Cancel listener", Toast.LENGTH_LONG).show()
+                listener.onDialogNegativeClick(this)
+            }
+
+            bSave.setOnClickListener {
+                Toast.makeText(context, "Save listener", Toast.LENGTH_LONG).show()
+                listener.onDialogPositiveClick(this)
+            }
+
+            builder.setView(dialogView)
+            builder.setCancelable(false)
+
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null!")
+    }
+}
