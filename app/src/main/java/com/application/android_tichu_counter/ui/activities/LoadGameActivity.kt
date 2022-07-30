60a2be45ac6448996d47c04f79dacf9a3b17b8b9
Fabian Diemand <fabian.diemand@swisscom.com>
2022-07-30 15:24:06 +0200

    feature(load-game-recycler): create layouts for load-game-screen and recycler items

---
+++ app/src/main/java/com/application/android_tichu_counter/ui/activities/LoadGameActivity.kt
@@ -0,0 +1,12 @@
+package com.application.android_tichu_counter.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.application.android_tichu_counter.R
+
+class LoadGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_load_game)
+    }
+}
