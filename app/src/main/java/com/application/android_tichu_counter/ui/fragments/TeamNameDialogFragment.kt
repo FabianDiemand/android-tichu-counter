4b8bf7cf38892aa6851b9f3abf0775329a1241f9
FabianDiemand <fabian.diemand@hotmail.com>
2022-08-06 11:28:42 +0200

    feature(view-binding): introduce view binding in teamname dialog

--- app/src/main/java/com/application/android_tichu_counter/ui/fragments/TeamNameDialogFragment.kt
+++ app/src/main/java/com/application/android_tichu_counter/ui/fragments/TeamNameDialogFragment.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.widget.ImageButton
 import androidx.fragment.app.DialogFragment
 import com.application.android_tichu_counter.R
+import com.application.android_tichu_counter.databinding.FragmentTeamNameDialogBinding
 import com.google.android.material.button.MaterialButton
 
 class TeamNameDialogFragment : DialogFragment() {
@@ -18,47 +19,50 @@ class TeamNameDialogFragment : DialogFragment() {
     }
 
     private lateinit var listener: TeamNameDialogListener
+    private var _binding: FragmentTeamNameDialogBinding? = null
 
-    interface TeamNameDialogListener{
+    private val binding get() = _binding!!
+
+    interface TeamNameDialogListener {
         fun onDialogSaveClicked(dialog: DialogFragment)
         fun onDialogBackClicked(dialog: DialogFragment)
     }
 
-    override fun onAttach(context: Context){
+    override fun onAttach(context: Context) {
         super.onAttach(context)
         Log.d(TAG, "Dialog attached.")
 
-        try{
+        try {
             listener = context as TeamNameDialogListener
             Log.d(TAG, "Listener instantiated.")
-        } catch(e: ClassCastException){
+        } catch (e: ClassCastException) {
             throw ClassCastException(("$context must implement TeamNameDialogListener."))
         }
     }
 
     @SuppressLint("InflateParams")
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        _binding = FragmentTeamNameDialogBinding.inflate(LayoutInflater.from(context))
+
+        setOnClickListeners()
+
         Log.d(TAG, "Create dialog.")
 
-        return activity?.let{
-            val builder = AlertDialog.Builder(it)
-            val dialogView = LayoutInflater.from(context).inflate(R.layout.fragment_team_name_dialog, null, false)
-
-            val bCancel = dialogView.findViewById<ImageButton>(R.id.ib_backbutton)
-            val bSave = dialogView.findViewById<MaterialButton>(R.id.b_save_teamnames)
-
-            bCancel.setOnClickListener {
-                listener.onDialogBackClicked(this)
-            }
-
-            bSave.setOnClickListener {
-                listener.onDialogSaveClicked(this)
-            }
-
-            builder.setView(dialogView)
-            builder.setCancelable(false)
-
-            builder.create()
+        return activity?.let {
+            AlertDialog.Builder(it)
+                .setView(binding.root)
+                .setCancelable(false)
+                .create()
         } ?: throw IllegalStateException("Activity cannot be null!")
+    }
+
+    private fun setOnClickListeners() {
+        binding.ibBackbutton.setOnClickListener {
+            listener.onDialogBackClicked(this)
+        }
+
+        binding.bSaveTeamnames.setOnClickListener {
+            listener.onDialogSaveClicked(this)
+        }
     }
 }
